02341ef2c663138d83827ec912d082abb7d5ea1e
y2k <git@y2kbugger.com>
2022-06-09 21:58:19 -0400

    All scripts get hashbang

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 pushd ./api-service
     export FLASK_APP=app
     pipenv run python -m flask run --without-threads
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 pushd ./api-service
     pipenv run python -m pytest
 popd
