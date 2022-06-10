067d08c024f98b92976c51cae66a682be79a06ba
y2k <git@y2kbugger.com>
2022-06-09 22:09:30 -0400

    Improve  OpenSSLService tests

--- api-service/opensslserver_test.py
+++ api-service/opensslserver_test.py
@@ -2,16 +2,21 @@ import pytest
 
 import opensslkeys
 from opensslserver import OpenSslServer
+from test_helpers import process_is_running
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def openssl_server():
     opensslkeys.ensure_generated()
     s = OpenSslServer()
     yield s
     s.stop()
 
-def test_server_cant_start_without_keys(openssl_server:OpenSslServer):
+def test_server_can_start(openssl_server:OpenSslServer):
+    openssl_server.ensure_started()
+    assert process_is_running('openssl', required_args=['s_server'])
+
+def test_server_cant_start_without_keys():
     opensslkeys.purge()
     with pytest.raises(RuntimeError, match="Failed to start `openssl"):
-        openssl_server.ensure_started()
+        OpenSslServer().ensure_started()
