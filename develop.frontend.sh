a9d778bd78d2a52737da5223917777f795ec2ecd
y2k <git@y2kbugger.com>
2022-06-22 17:43:57 -0400

    Add explaination to avoid wasting time in the future on streamlining developer launcher.

--- develop.frontend.sh
+++ develop.frontend.sh
@@ -1,6 +1,15 @@
 set -e
 
-pushd ./frontend
-    export ASPNETCORE_ENVIRONMENT=Development
+KATSU_PROJECT_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+pushd $KATSU_PROJECT_ROOT/frontend
+    # As of writing this, dotnet cli has the following quirks:
+    # Env variables here don't have an effect when using dotnet watch
+    # - Dotnet watch will utilize the default profile in launchSettings.json
+    # - You can specify a alternate profile via:
+    #   `dotnet watch run --launch-profile=Frontend.Develop`
+    #   but then hot reload fails to work.
+    # - You can configure the profile to use http only, because
+    #   whole app is local....but again, that causes hot reload to stop working
     dotnet watch
 popd
