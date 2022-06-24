57b6e98d612ad9bf0d21ef5e1eaa08a007afe5f6
y2k <git@y2kbugger.com>
2022-06-24 08:09:33 -0400

    Use pre-built frontend in start script

--- pykatsu/src/pykatsu/api_service/host_frontend.py
+++ pykatsu/src/pykatsu/api_service/host_frontend.py
@@ -1,16 +1,28 @@
+import os
+from pathlib import Path
+
 from flask import redirect, send_from_directory, url_for
 
 from .flaskapp import app
 
+KATSU_PROJECT_ROOT = Path(os.environ['KATSU_PROJECT_ROOT'])
+FRONTEND_BLAZOR_OUTPUT_PATH = KATSU_PROJECT_ROOT / "frontend-blazor-output" / "wwwroot"
+if FRONTEND_BLAZOR_OUTPUT_PATH.exists():
+    print("Serving frontend-blazor using flask.\n")
+    @app.route('/')
+    def start_frontend():
+        return send_from_directory(FRONTEND_BLAZOR_OUTPUT_PATH, "index.html")
 
-@app.route('/')
-def start_frontend():
-    return send_from_directory("../frontend/bin/Debug/net6.0/publish/wwwroot", "index.html")
+    @app.route('/<path:path>')
+    def serve_frontend_assets(path):
+        return send_from_directory(FRONTEND_BLAZOR_OUTPUT_PATH, path)
 
-@app.route('/<path:path>')
-def serve_frontend_assets(path):
-    return send_from_directory("../frontend/bin/Debug/net6.0/publish/wwwroot", path)
-
-@app.errorhandler(404)
-def redirect_back_to_blazor_startup(e):
-    return redirect(url_for('start_frontend'), code=301)
+    @app.errorhandler(404)
+    def redirect_back_to_blazor_startup(e):
+        return redirect(url_for('start_frontend'), code=301)
+else:
+    print(
+        "Not serving frontend-blazor using flask.",
+        "Either you are a developer and running frontend-blazor separately.",
+        "Otherwise something went wrong, please contact support.",
+        sep='\n')
--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
 export KATSU_PROJECT_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Get prebuilt frontend
+FRONTEND_ARCHIVE='http://github.com/radical-semiconductor/katsu-board-demo/releases/latest/download/frontend-blazor.tar.gz'
+FRONTEND_ARCHIVE_OUTPUT=blazor-frontent-output
+if [ ! -d "$FRONTEND_ARCHIVE_OUTPUT" ]; then
+    curl --silent --show-error --location $FRONTEND_ARCHIVE | tar zx
+fi
+
 pushd $KATSU_PROJECT_ROOT/pykatsu
+    pipenv sync
+    pipenv graph
     export FLASK_APP=pykatsu.api_service.flaskapp
     pipenv run python -m flask run --without-threads
 popd
