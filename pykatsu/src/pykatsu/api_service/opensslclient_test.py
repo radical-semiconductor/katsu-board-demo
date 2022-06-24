87d001f7c30e95a6fe7ec571579e1a131775e571
y2k <git@y2kbugger.com>
2022-06-24 11:56:56 -0400

    Also debug out server side when client fails to connect

--- pykatsu/src/pykatsu/api_service/opensslclient_test.py
+++ pykatsu/src/pykatsu/api_service/opensslclient_test.py
@@ -7,7 +7,7 @@ from .test_helpers import process_is_running
 
 
 @pytest.fixture(scope='session')
-def openssl_client():
+def openssl_client_server():
     opensslkeys.ensure_generated()
     port = 15000
     s = OpenSslServer(port)
@@ -15,12 +15,19 @@ def openssl_client():
     import time
     time.sleep(1)
     c = OpenSslClient(port)
-    yield c
+    yield c, s
     c.stop()
     s.stop()
 
-def test_client_can_start(openssl_client:OpenSslClient):
-    openssl_client.ensure_started()
+def test_client_can_start(openssl_client_server):
+    c, s = openssl_client_server
+    try:
+        c.ensure_started()
+    except Exception as e:
+        print("Client connection failed.")
+        print("Below is output from server side:")
+        print(s.read(200))
+        raise e
     # Can't check in the normal way because openssl is either purposely
     # inadvertantly scrubbing argv from the process
     # assert process_is_running('openssl', required_args=['s_client'])
