290bbbd889ecaba48972681b5bd95f1a1f9551fe
y2k <git@y2kbugger.com>
2022-07-02 19:21:47 -0400

    Improve existing develop scripts
    Make is so that develop vscode tasks don't need fully relaunched upon build failure
    
    Previously if the tool native watcher, e.g. flask debug, died the
    Task would need to be relaunched

--- develop.api-service.sh
+++ develop.api-service.sh
@@ -2,10 +2,20 @@ set -e
 
 export KATSU_PROJECT_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Configure
+export FLASK_APP=pykatsu.api_service.flaskapp
+export FLASK_ENV=development
 pushd $KATSU_PROJECT_ROOT/pykatsu
     pipenv install --dev
     pipenv graph
-    export FLASK_APP=pykatsu.api_service.flaskapp
-    export FLASK_ENV=development
-    pipenv run python -m flask run --without-threads --port 8284
 popd
+
+# Build loop, incase you make an edit that crashs flask
+while :; do
+    pushd $KATSU_PROJECT_ROOT/pykatsu
+        pipenv run python -m flask run --without-threads --port 8284 || true
+    popd
+
+    printf "\n### Watching for edits to pykatsu ###\n"
+    inotifywait -r -e close_write "$KATSU_PROJECT_ROOT/pykatsu"
+done
--- develop.frontend.sh
+++ develop.frontend.sh
@@ -2,14 +2,18 @@ set -e
 
 export KATSU_PROJECT_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-pushd $KATSU_PROJECT_ROOT/frontend
-    # As of writing this, dotnet cli has the following quirks:
-    # Env variables here don't have an effect when using dotnet watch
-    # - Dotnet watch will utilize the default profile in launchSettings.json
-    # - You can specify a alternate profile via:
-    #   `dotnet watch run --launch-profile=Frontend.Develop`
-    #   but then hot reload fails to work.
-    # - You can configure the profile to use http only, because
-    #   whole app is local....but again, that causes hot reload to stop working
-    dotnet watch
-popd
+# Build loop, incase you want to clean start dotnet watch. Use CTRL-C in terminal for this
+while :; do
+    pushd $KATSU_PROJECT_ROOT/frontend
+        # As of writing this, dotnet cli has the following quirks:
+        # Env variables here don't have an effect when using dotnet watch
+        # - Dotnet watch will utilize the default profile in launchSettings.json
+        # - You can specify a alternate profile via:
+        #   `dotnet watch run --launch-profile=Frontend.Develop`
+        #   but then hot reload fails to work.
+        # - You can configure the profile to use http only, because
+        #   whole app is local....but again, that causes hot reload to stop working
+        dotnet watch
+    popd
+    inotifywait -r -e close_write "$KATSU_PROJECT_ROOT/frontend"
+done
