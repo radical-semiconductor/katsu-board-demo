6bd69f202db1ccf4aebff162ce5304adf31fd141
y2k <git@y2kbugger.com>
2022-07-03 02:11:06 -0400

    Use the correct openssl paths on windows

--- pykatsu/src/pykatsu/api_service/openssl.py
+++ pykatsu/src/pykatsu/api_service/openssl.py
@@ -1,10 +1,19 @@
 import os
+import sys
 
 from pykatsu.projectpath import KATSU_PROJECT_ROOT
 
 OPENSSL_ROOT_PATH = KATSU_PROJECT_ROOT / 'openssl-output'
-OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
-OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
+if sys.platform.startswith('linux'):
+    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
+    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
+elif sys.platform.startswith('win'):
+    OPENSSL_ROOT_PATH = OPENSSL_ROOT_PATH / 'Program Files' / 'OpenSSL'
+    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH  / 'bin' / 'openssl.exe'
+    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
+else:
+    raise NotImplementedError()
+
 
 OPENSSL_ENV = os.environ.copy()
 OPENSSL_ENV["OPENSSL_CONF"] = str(OPENSSL_CONFIG_PATH)
