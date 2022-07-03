d515a58a06b7271ae2559673876c5c84897842af
y2k <git@y2kbugger.com>
2022-07-02 22:20:22 -0400

    Use prebuild openssl in start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,18 @@ set -e
 
 export KATSU_PROJECT_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-# Get prebuilt frontend
+# Get prebuilt frontend-blazor
 FRONTEND_ARCHIVE='http://github.com/radical-semiconductor/katsu-board-demo/releases/latest/download/frontend-blazor.tar.gz'
-FRONTEND_ARCHIVE_OUTPUT=blazor-frontent-output
+FRONTEND_ARCHIVE_OUTPUT=blazor-frontend-output
 if [ ! -d "$FRONTEND_ARCHIVE_OUTPUT" ]; then
     curl --silent --show-error --location $FRONTEND_ARCHIVE | tar zx
+fi
+
+# Get prebuilt openssl
+OPENSSL_ARCHIVE='http://github.com/radical-semiconductor/katsu-board-demo/releases/latest/download/openssl.linux-x86_64.tar.gz'
+OPENSSL_ARCHIVE_OUTPUT=openssl-output
+if [ ! -d "$OPENSSL_ARCHIVE_OUTPUT" ]; then
+    curl --silent --show-error --location $OPENSSL_ARCHIVE | tar zx
 fi
 
 pushd $KATSU_PROJECT_ROOT/pykatsu
