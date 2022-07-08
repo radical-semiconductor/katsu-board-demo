5dd65087a62784a38f72b7386860749d5a30c18f
y2k <git@y2kbugger.com>
2022-07-08 18:23:46 -0400

    Implement darwin OpenSSL path

--- pykatsu/src/pykatsu/api_service/openssl.py
+++ pykatsu/src/pykatsu/api_service/openssl.py
@@ -7,12 +7,17 @@ OPENSSL_ROOT_PATH = KATSU_PROJECT_ROOT / 'openssl-output'
 if sys.platform.startswith('linux'):
     OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
     OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
+elif sys.platform.startswith('darwin'):
+    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
+    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
 elif sys.platform.startswith('win'):
     OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'Program Files' / 'OpenSSL' / 'bin' / 'openssl.exe'
     OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'Program Files' / 'Common Files' / 'SSL' / 'openssl.cnf'
 else:
-    raise NotImplementedError()
+    raise NotImplementedError("Unknown platform. Unable to know path to openssl binaries")
 
+if not OPENSSL_BIN_PATH.exists():
+    print(f"Warning, openssl binary doesn't exist at `{str(OPENSSL_BIN_PATH)}`")
 
 OPENSSL_ENV = os.environ.copy()
 OPENSSL_ENV["OPENSSL_CONF"] = str(OPENSSL_CONFIG_PATH)
