f2da284b2cb71597c8f4e6fe4af3bfae01f4e55f
y2k <git@y2kbugger.com>
2022-07-17 15:06:12 -0400

    Auto open the browser when using the start script.

--- pykatsu/src/pykatsu/api_service/flaskapp.py
+++ pykatsu/src/pykatsu/api_service/flaskapp.py
@@ -1,6 +1,17 @@
+import os
+import webbrowser
+
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 from . import api, host_frontend
+
+try:
+    FLASK_RUN_PORT = int(os.environ['FLASK_RUN_PORT'])
+    webbrowser.open(f'http://127.0.0.1:{FLASK_RUN_PORT}/')
+except:
+    print("Error while trying to open app in a browser.")
+    # We are also expected to get here when using the standard
+    # development scripts, the browser is opened by blazor dev loop
