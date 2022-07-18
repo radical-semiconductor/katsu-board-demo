8a13bd38aa4f419a9513d9324bd4a39b5c5f6161
y2k <git@y2kbugger.com>
2022-07-17 23:00:40 -0400

    Fix path for OpenSSL binaries

--- pykatsu/src/pykatsu/api_service/openssl.py
+++ pykatsu/src/pykatsu/api_service/openssl.py
@@ -4,17 +4,11 @@ import sys
 from pykatsu.projectpath import KATSU_PROJECT_ROOT
 
 OPENSSL_ROOT_PATH = KATSU_PROJECT_ROOT / 'openssl-output'
-if sys.platform.startswith('linux'):
-    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
-    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
-elif sys.platform.startswith('darwin'):
-    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
-    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
-elif sys.platform.startswith('win') or str(sys.executable).lower().endswith('exe'):
-    OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'Program Files' / 'OpenSSL' / 'bin' / 'openssl.exe'
-    OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'Program Files' / 'Common Files' / 'SSL' / 'openssl.cnf'
-else:
-    raise NotImplementedError("Unknown platform. Unable to know path to openssl binaries")
+OPENSSL_BIN_PATH = OPENSSL_ROOT_PATH / 'bin' / 'openssl'
+OPENSSL_CONFIG_PATH = OPENSSL_ROOT_PATH / 'ssl' / 'openssl.cnf'
+
+if sys.platform.startswith('win') or str(sys.executable).lower().endswith('exe'):
+    OPENSSL_BIN_PATH = OPENSSL_BIN_PATH.with_suffix('.exe')
 
 if not OPENSSL_BIN_PATH.exists():
     print(f"Warning, openssl binary doesn't exist at `{str(OPENSSL_BIN_PATH)}`")
